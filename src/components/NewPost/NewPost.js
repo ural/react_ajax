0bfbf7ed0c3ba0fc62afb155b8ba60bdb74011c6
ural <sevomax@gmail.com>
2018-03-15 14:31:30 -0400

    new post js

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -7,6 +8,20 @@ class NewPost extends Component {
     title: '',
     content: '',
     author: 'Max'
+  };
+
+  postDataHandler = () => {
+
+    const newPost = {
+      title: this.state.title,
+      body: this.state.content,
+      author: this.state.author
+    };
+
+    axios.post('https://jsonplaceholder.typicode.com/posts', newPost)
+      .then(response => {
+        console.log(response);
+      });
   };
 
   render() {
@@ -21,9 +36,9 @@ class NewPost extends Component {
         <label>Author</label>
         <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
           <option value="Max">Max</option>
-          <option value="Manu">Manu</option>
+          <option value="Manush">Manush</option>
         </select>
-        <button>Add Post</button>
+        <button onClick={this.postDataHandler}>Add Post</button>
       </div>
     );
   }
