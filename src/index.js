b8194f59c15949b74ade8c865a6795bcc541505e
ural <sevomax@gmail.com>
2018-03-15 19:59:15 -0400

    playing with axios.default.headers 2

--- src/index.js
+++ src/index.js
@@ -9,11 +9,9 @@ axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEn FROM ME';
 axios.defaults.headers.common['WHAAAATTTT'] = 'SAY WHAAAAT!!!!';
 axios.defaults.headers.common['User-Agent'] = 'dAH FOX';
-/*
-axios.defaults.headers.common['I can put anything in here'] = 'ANYTHING AT ALL ???';
-*/
+axios.defaults.headers.common['I_can_put_anything_in_here'] = 'ANYTHING AT ALL ???';
+axios.defaults.xsrfCookieName = 'Whats_this_cookie';
 
-console.log(axios.defaults.headers);
 
 let requerstInterceptor = axios.interceptors.request.use(request => {
   console.log(request);
